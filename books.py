f0f8822a92450e840fb98de7162d6350cc6b1e8c
max <max@theguards.net>
2023-06-03 16:39:59 -0700

    massive overhaul of script, now dumps markdown files

--- books.py
+++ books.py
@@ -2,13 +2,18 @@
 
 '''
 Author: Max Russell, max@theguards.net
-tool to work with openlibary api 
-goal is to generate a html file formatted to look nice from a list of ISBN's 
+tool to work with openlibary api
+goal is to generate a html file formatted to look nice from a list of ISBN's
 for use on a web server or other viewer
 '''
 import requests
 import json
 import argparse
+import re
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
+from unidecode import unidecode
+import os
 from bs4 import BeautifulSoup
 
 parser = argparse.ArgumentParser()
@@ -18,66 +23,97 @@ args= parser.parse_args()
 
 #isbn_str = "055329461X"
 
-def get_book_info():
-	#uses the ISBN to get information on book from openlibrary 
-	#isbn = str(9781492051367)
-	#get complete payload
-	payload = {'bibkeys': 'ISBN:' + args.isbn_str, 'format': 'json', 'jscmd': 'details'}
-	r = requests.get('https://openlibrary.org/api/books', params=payload)
-	y = r.json()
-	
-	global general_json
-	global detail_json
-	general_json = y['ISBN:' + args.isbn_str]
-	detail_json = general_json['details']
-
-def get_book_description():
-    '''uses the info_url to find the web page, parse it out to get the 
-    description
-    '''
-    book_url = general_json["info_url"]
-    #print(book_url)
-    book_webpage = requests.get(book_url)
-    #print(book_webpage)
-    webpage_content = book_webpage.text
-    #print(webpage_content)
-    content = BeautifulSoup(webpage_content, 'lxml')
-    #print(content)
-    #description = content.find_all('div', attrs={'class':'workHelp'})
-    description = content.find_all('p', attrs={'class':'workHelp'})
-    #description = content.find_all('div', attrs={'class':'book-description-content restricted-view'})
-    print(description)
-
 class Book():
-	#load up a book with data from the API
-	def __init__(self, isbn):
-		self.isbn = isbn
-		self.title = detail_json["title"]
-		self.numOfPages = detail_json["number_of_pages"]
-		self.publishDate = detail_json["publish_date"]
-		self.authors_json = detail_json["authors"]
-		self.authors_first = self.authors_json[0]
-		self.authors = self.authors_first["name"]
-		
-		
+    #load up a book with data from the API
+    def __init__(self, isbn):
+        self.isbn = isbn
+        self.get_book_info()
+        self.title = self.general_json["title"]
+        self.url = self.general_json["url"]
+        try:
+            self.pages = self.general_json["number_of_pages"]
+        except:
+            try:
+                self.pages = self.general_json["pagination"]
+            except:
+                self.pages = ""
+        try:
+            self.year = self.general_json["publish_date"]
+        except:
+            self.year = ""
+        authors_json = self.general_json["authors"]
+        authors_list = []
+        for value in authors_json:
+            authors_list.append(value["name"])
+        if len(authors_list) == 1:
+            self.authors = authors_list[0]
+        else:
+            self.authors = ", ".join(authors_list)
+        self.get_book_description()
+
+    def get_book_info(self):
+        payload = {'bibkeys': 'ISBN:' + self.isbn, 'format': 'json', 'jscmd': 'data'}
+        r = requests.get('https://openlibrary.org/api/books', params=payload)
+        y = r.json()
+        self.general_json = y['ISBN:' + args.isbn_str]
+
+    def get_book_description(self):
+        '''uses the info_url to find the web page, parse it out to get the
+        description
+        '''
+        book_url = self.general_json["url"]
+        book_webpage = requests.get(book_url)
+        webpage_content = book_webpage.text
+        content = BeautifulSoup(webpage_content, 'lxml')
+        description = content.find_all('div', attrs={'class':'book-description-content restricted-view'})
+        html = BeautifulSoup(str(description), 'lxml')
+        text = html.find_all('p')
+        desc_list = []
+        for count, paragraph in enumerate(text):
+            black_list = ["<p>", "</p>", "\n"]
+            if count == 0:
+                pass
+            else:
+                for black in black_list:
+                    paragraph = str(paragraph).replace(black, '')
+                desc_list.append(unidecode(paragraph))
+        self.description = ''.join(desc_list)
+
+    def render_template(self):
+        '''uses a book object and template to create a post file'''
+        now = datetime.now()
+        now_formatted = now.strftime("%Y-%m-%d %H:%M:%S -0700")
+        environment = Environment(loader=FileSystemLoader("templates/"))
+        template = environment.get_template("book.j2")
+        filename = self.isbn + "_post.markdown"
+        rendered = template.render(title=self.title, 
+                                    date=now_formatted,
+                                    isbn=self.isbn,
+                                    authors=self.authors,
+                                    year=self.year,
+                                    pages=self.pages,
+                                    description=self.description,
+                                    url=self.url)
+        with open(filename, mode="w", encoding="utf-8") as post:
+            post.write(rendered)
+    
+
 def main():
-	#main script starts here
-	#print(z['authors'])
-	get_book_info()
-	# print(str(general_json) + "\n" + "\n")
-	# print(detail_json)
-	# print('now for the show...' + '\n' + '\n')
-	thebook = Book(args.isbn_str)
-	print(thebook.title)
-	print(thebook.isbn)
-	print(thebook.authors)
-	print(thebook.publishDate)
-	print(str(thebook.numOfPages) + " pages")
-	get_book_description()
+    #main script starts here
+    if os.path.isfile(args.isbn_str + "_post.markdown"):
+        print("exists, skipping ", args.isbn_str)
+    else:
+        thebook = Book(args.isbn_str)
+        print(thebook.title)
+        print(thebook.isbn)
+        print(thebook.authors)
+        print(thebook.year)
+        print(str(thebook.pages) + " pages")
+        thebook.render_template()
 
 try:
-	args.isbn_str
+    args.isbn_str
 except:
-	parser.print_help()
+    parser.print_help()
 
 main()
